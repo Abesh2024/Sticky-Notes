aef21ed8ae3641b7e0d2cc4b3473e45a9bc48763
Abesh <abeshsarkar2030@gmail.com>
2024-01-17 15:00:42 +0530

    some changes made

--- script.js
+++ script.js
@@ -1,34 +1,51 @@
+
+const button2 = document.querySelector(".clear-all");
+
 let noteColor = document.querySelector("#note-color").children;
 let notes = document.querySelector("#show-notes")
 let color = document.querySelector("#color");
-
 let btn = document.querySelector("button");
+let text = document.querySelector("#remove-text");
 
-btn.addEventListener("click", ()=>{
+btn.addEventListener("click", () => {
     let note = noteColor[0].value;
     // let color = noteColor[1].value;
     // console.log(note, color.value);
-    if(note === ""){
+    if (note === "") {
         alert("please enter some text!");
     }
+    stickyNotes(note, color.value);
 
-     stickyNotes (note, color.value);
-
-     noteColor[0].value = "";
-     document.querySelector("#remove-text").style.display = "none";
-
+    noteColor[0].value = "";
+    text.style.display = "none";
 })
 
 
-function stickyNotes (note, color) {
+function stickyNotes(note, color) {
     let div = document.createElement("div");
     div.style.height = "150px";
     div.style.width = "150px";
 
     div.innerHTML = `
-    <p>${note}</p>
-    <button>X</button>
-    `
+<p>${note}</p>
+<button class="btn2">X</button>
+`
     div.style.backgroundColor = color;
     notes.appendChild(div);
 }
+
+notes.addEventListener("click", (e) => {
+    if (e.target.tagName === "BUTTON") {
+        e.target.parentElement.remove();
+        if (notes.innerHTML === "") {
+           text.style.display = "block";
+        }
+    }
+})
+
+button2.addEventListener("click", (e) => {
+
+    notes.innerHTML = ``;
+    text.style.display = "block";
+
+})
